578bd35da38733d3456747b43ccd3792d67f24b7
Ronald Gerits <ronald.gerits@gmail.com>
2023-03-14 22:39:07 +0100

    Finish nav, start header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,18 +30,69 @@
 <body class="antialiased">
         <div class="text-gray-900 text-sm">
             <nav class="bg-gray-900 text-white px-4 py-3 flex items-center justify-between">
-                <div class="flex-items-center space-x-4">
+                <div class="flex items-center space-x-4">
                     <a href="#" class="text-white hover:text-gray-400">
                         <svg aria-hidden="true" viewBox="0 0 16 16"  data-view-component="true" class="w-8 fill-current">
                             <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
                         </svg>
                     </a>
                     <div class="relative">
-                        <input type="text" class="rounded bg-gray-700 placeholder-white w-72 px-3 py-1" placholder="Search or jump to...">
+                        <input type="text" class="rounded bg-gray-700 placeholder-white w-72 px-3 py-1" placeholder="Search or jump to...">
+                        <div class="absolute top-0 right-0 flex items-center h-full">
+                            <div class="border border-gray-600 rounded text-xs text-gray-400 px-2 mr-2">/</div>
+                        </div>
+                    </div>
+                    <ul class="flex items-center font-semibold space-x-4">
+                        <li><a href="#" class="hover:text-gray-400">Pull Requests</a></li>
+                        <li><a href="#" class="hover:text-gray-400">Issues</a></li>
+                        <li><a href="#" class="hover:text-gray-400">Marketplace</a></li>
+                        <li><a href="#" class="hover:text-gray-400">Explore</a></li>
+                    </ul>
+                </div>
+                <div class="flex items-center space-x-4 text-white">
+                    <a href="#" class="relative hover:text-gray-400">
+                        <svg class="w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                        </svg>
+                        <span class="bg-blue-600 rounded-full w-2 h-2 absolute top-0 right-0"></span>
+                    </a>
+                    <a href="#" class="flex items-center hover:text-gray-400">
+                        <svg class="w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                        <svg class="w-3 ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
+
+                    </a>
+                    <a href="#" class="flex items-center hover:text-gray-400">
+                        <img class="rounded-full w-5 h-5" src="https://avatars.githubusercontent.com/u/24208222?s=40&v=4">
+                        <svg class="w-3 ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </a>
+                </div>
+            </nav>
+            <div class="repo-stats flex items-center justify-between px-8 py-4">
+                <div class="flex">
+                    <svg class="w-4 fill-current text-gray-600" aria-hidden="true" viewBox="0 0 16 16" data-view-component="true">
+                        <path d="M4 4a4 4 0 0 1 8 0v2h.25c.966 0 1.75.784 1.75 1.75v5.5A1.75 1.75 0 0 1 12.25 15h-8.5A1.75 1.75 0 0 1 2 13.25v-5.5C2 6.784 2.784 6 3.75 6H4Zm8.25 3.5h-8.5a.25.25 0 0 0-.25.25v5.5c0 .138.112.25.25.25h8.5a.25.25 0 0 0 .25-.25v-5.5a.25.25 0 0 0-.25-.25ZM10.5 6V4a2.5 2.5 0 1 0-5 0v2Z"></path>
+                    </svg>
+                    <div class="flex items-center text-xl ml-2">
+                        <a href="#" class="text-blue-600 hover:underline">rgerits</a>
+                        <span class="ml-1">/</span>
+                        <a href="#" class="text-blue-600 hover:underline ml-1 font-semibold">rgerits</a>
                     </div>
                 </div>
-                <div>right</div>
-            </nav>
+                <div class="flex space-x-2 ">
+                    <div class="flex text-sm">
+                        <a href="#" class="border border-gray-400 rounded rounded-r-none px-3 py-1 flex items-centered space-x-1">
+
+                        </a>
+                        <a href="#" class="border border-gray-400 rounded rounded-l-none border-l-0 px-3 py-1 font-semibold hover:text-blue-600">430</a>
+                    </div>
+                </div>
+            </div> <!-- end repo stats -->
         </div>
 </body>
 </html>
