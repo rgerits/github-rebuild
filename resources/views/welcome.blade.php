a63ad2e0f4c62bc15b0cda62024c589eba037ebe
Ronald Gerits <ronald.gerits@gmail.com>
2023-03-20 21:09:28 +0100

    File explorer WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -153,7 +153,44 @@
                         <span class="ml-2">Pull requests</span>
                     </a>
                 </li>
-            </ul>
+            </ul> <!-- end rep nav -->
+            <div class="container mx-auto my-8 px-4 flex">
+                <div class="file-explorer-container w-3/4 mr-8">
+                    <div class="branch-navigation flex items-center justify-between">
+                        <div class="flex items-center space-x-2">
+                            <button class="border border-gray-400 rounded-md px-4 py-1 hover:bg-gray-200 flex items-center space-x-2">
+                                <svg text="gray" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-git-branch">
+                                    <path d="M9.5 3.25a2.25 2.25 0 1 1 3 2.122V6A2.5 2.5 0 0 1 10 8.5H6a1 1 0 0 0-1 1v1.128a2.251 2.251 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.5 0v1.836A2.493 2.493 0 0 1 6 7h4a1 1 0 0 0 1-1v-.628A2.25 2.25 0 0 1 9.5 3.25Zm-6 0a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Zm8.25-.75a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM4.25 12a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Z"></path>
+                                </svg>
+                                <span>develop</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </button>
+                            <a href="#" class="flex items-center space-x-2 px-3 py-1 group">
+                                <svg text="gray" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="fill-current text-gray-600 group-hover:text-blue-600 w-4">
+                                    <path d="M9.5 3.25a2.25 2.25 0 1 1 3 2.122V6A2.5 2.5 0 0 1 10 8.5H6a1 1 0 0 0-1 1v1.128a2.251 2.251 0 1 1-1.5 0V5.372a2.25 2.25 0 1 1 1.5 0v1.836A2.493 2.493 0 0 1 6 7h4a1 1 0 0 0 1-1v-.628A2.25 2.25 0 0 1 9.5 3.25Zm-6 0a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Zm8.25-.75a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5ZM4.25 12a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Z"></path>
+                                </svg>
+                                <span class="font-semibold group-hover:text-blue-600">3</span>
+                                <span class="group-hover:text-blue-600">branches</span>
+                            </a>
+                            <a href="#" class="flex items-center space-x-2 px-3 py-1 group">
+                                <svg text="gray" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="fill-current text-gray-600 group-hover:text-blue-600 w-4">
+                                    <path d="M1 7.775V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 0 1 0 2.474l-5.026 5.026a1.75 1.75 0 0 1-2.474 0l-6.25-6.25A1.752 1.752 0 0 1 1 7.775Zm1.5 0c0 .066.026.13.073.177l6.25 6.25a.25.25 0 0 0 .354 0l5.025-5.025a.25.25 0 0 0 0-.354l-6.25-6.25a.25.25 0 0 0-.177-.073H2.75a.25.25 0 0 0-.25.25ZM6 5a1 1 0 1 1 0 2 1 1 0 0 1 0-2Z"></path>
+                                </svg>
+                                <span class="font-semibold group-hover:text-blue-600">0</span>
+                                <span class="group-hover:text-blue-600">tags</span>
+                            </a>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            two
+                        </div>
+                    </div>
+                </div>
+                <div class="sidebar w-1/4">
+
+                </div>
+            </div>
         </div>
 </body>
 </html>
