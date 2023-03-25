2098af78eb4919310d40039494681df076e9fc2e
Ronald Gerits <ronald.gerits@gmail.com>
2023-03-25 15:54:02 +0100

    Fix second column

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -183,29 +183,30 @@
                             </a>
                         </div>
                         <div class="flex items-center space-x-2">
-                            <div class="sidebar w-1/4 flex items-center">
-                                <button class="border border-gray-400 rounded-md text-white hover:bg-green-600 px-4 py-1 flex items-center space-x-2">
-                                    <div>Go to file</div>
-                                </button>
-                                <button class="border border-gray-400 rounded-md text-white bg-green-500 hover:bg-green-600 px-4 py-1 flex items-center space-x-2">
+                            <button class="border border-gray-400 rounded-md px-4 py-1 hover:bg-gray-200 flex items-center space-x-2">
+                                <div>Go to file</div>
+                            </button>
+                            <button class="border border-gray-400 rounded-md px-4 py-1 hover:bg-gray-200 flex items-center space-x-2">
 
-                                    <div>Add file</div>
-                                    <svg class="w-4 pt-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                    </svg>
-                                </button>
-                                <button class="border border-gray-400 rounded-md text-white bg-green-500 hover:bg-green-600 px-4 py-1 flex items-center space-x-2">
-                                    <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="text-white fill-current">
-                                        <path d="m11.28 3.22 4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.275-.326.749.749 0 0 1 .215-.734L13.94 8l-3.72-3.72a.749.749 0 0 1 .326-1.275.749.749 0 0 1 .734.215Zm-6.56 0a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042L2.06 8l3.72 3.72a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L.47 8.53a.75.75 0 0 1 0-1.06Z"></path>
-                                    </svg>
-                                    <div>Code</div>
-                                    <svg class="w-4 pt-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                    </svg>
-                                </button>
-                            </div>
+                                <div>Add file</div>
+                                <svg class="w-4 pt-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </button>
+                            <button class="border border-gray-400 rounded-md text-white bg-green-500 hover:bg-green-600 px-4 py-1 flex items-center space-x-2">
+                                <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="text-white fill-current">
+                                    <path d="m11.28 3.22 4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.275-.326.749.749 0 0 1 .215-.734L13.94 8l-3.72-3.72a.749.749 0 0 1 .326-1.275.749.749 0 0 1 .734.215Zm-6.56 0a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042L2.06 8l3.72 3.72a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L.47 8.53a.75.75 0 0 1 0-1.06Z"></path>
+                                </svg>
+                                <div>Code</div>
+                                <svg class="w-4 pt-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </button>
                         </div>
                     </div>
+                </div>
+                <div class="sidebar w-1/4 flex items-center">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolorum praesentium quam quo, sapiente sunt velit veritatis! Ab, culpa cupiditate deserunt expedita laudantium maxime molestiae quisquam reiciendis sit suscipit unde!
                 </div>
             </div>
         </div>
